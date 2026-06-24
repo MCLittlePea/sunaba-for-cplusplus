80eae39901f586e42a88340d99e6e61d9e0b4c65
MCLittlePea <203047590@qq.com>
2026-06-24 13:59:28 +0800

    未编写主库，其它均完成

--- sunaba-for-cplusplus.h
+++ sunaba-for-cplusplus.h
@@ -3,4 +3,22 @@
 
 #include "pixel_render.h"
 
+class SunabaProgram
+{
+private:
+    static constexpr int MXmem = 70000;
+    int mem[MXmem];
+
+public:
+    SunabaProgram() = default;
+    SunabaProgram(const SunabaProgram&) = default;
+    SunabaProgram& operator=(const SunabaProgram&) = default;
+    ~SunabaProgram() = default;
+
+    int operator[](int i)
+    {
+        
+    }
+};
+
 #endif
